417b17e1a61cfa9bf5dbcc74181737d16c3fa33c
heebum99 <heebum9955@naver.com>
2022-10-04 13:13:54 +0900

    1001번 문제 - 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.

---
+++ src/Bronz5/Q1001.java
@@ -0,0 +1,14 @@
+package Bronz5;
+
+import java.util.Scanner;
+
+public class Q1001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A-B);
+    }
+}
