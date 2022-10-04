2700517b7a0bd95bbcab34dc02b552e211f0b3e9
heebum99 <heebum9955@naver.com>
2022-10-04 13:31:11 +0900

    1330번 문제 - 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.

---
+++ src/Bronz5/Q1330.java
@@ -0,0 +1,23 @@
+package Bronz5;
+
+//-10,000 ¡Â A, B ¡Â 10,000
+
+import java.util.Scanner;
+
+public class Q1330 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        if(A > B){
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        }
+        else{
+            System.out.println("==");
+        }
+    }
+}
