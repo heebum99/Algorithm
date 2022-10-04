92e1a3cf3444503ca0cd7d2f3fb66bae76c62a0d
heebum99 <heebum9955@naver.com>
2022-10-04 13:17:20 +0900

    1008번 문제 - 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.

---
+++ src/Bronz5/Q1008.java
@@ -0,0 +1,15 @@
+package Bronz5;
+
+import java.util.Scanner;
+
+public class Q1008 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
+
+        System.out.println(A/B);
+        scanner.close();
+    }
+}
