56218c0226ecd365e7f80b39785959c775c21187
heebum99 <heebum9955@naver.com>
2022-10-11 16:09:48 +0900

    5339번 문제 - 스타워즈에 등장하는 로봇인 C3PO는 요즘 콜센터에 근무하고 있다. 콜센터에 앉아있는 C3PO를 그리는 프로그램을 작성하시오.

---
+++ src/Bronz5/Q5339.java
@@ -0,0 +1,16 @@
+package Bronz5;
+
+public class Q5339 {
+    public static void main(String[] args) {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.print("|          |");
+    }
+}
