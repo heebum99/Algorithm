f617a1d0c28c8249e77ebed3c2e6adb432370d08
heebum99 <heebum9955@naver.com>
2022-10-11 16:00:17 +0900

    5337번 문제 - Welcome을 예제 출력처럼 출력하는 프로그램을 작성하시오.

---
+++ src/Bronz5/Q5337.java
@@ -0,0 +1,16 @@
+package Bronz5;
+
+//Welcome을 아래 예제 출력처럼 출력한다.
+/*
+.  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/.
+*/
+
+public class Q5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.print("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
