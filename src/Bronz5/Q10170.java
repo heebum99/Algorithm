fab5fe78da9ef907078d12987680f90eaa0772eb
heebum99 <heebum9955@naver.com>
2022-10-12 16:06:33 +0900

    10170번 문제 - 동혁이를 위해 NFC 서부와 북부 디비전 순위를 출력하는 프로그램을 작성하시오.

---
+++ src/Bronz5/Q10170.java
@@ -0,0 +1,18 @@
+package Bronz5;
+
+//동혁이를 위해 NFC 서부와 북부 디비전 순위를 출력하는 프로그램을 작성하시오.
+
+public class Q10170 {
+    public static void main(String[] args) {
+        System.out.println("NFC West       W   L  T");
+        System.out.println("-----------------------");
+        System.out.println("Seattle        13  3  0");
+        System.out.print("San Francisco  12  4  0\n"+"Arizona        10  6  0\n"+"St. Louis      7   9  0\n" +"\n"+
+                "NFC North      W   L  T\n" +
+                "-----------------------\n" +
+                "Green Bay      8   7  1\n" +
+                "Chicago        8   8  0\n" +
+                "Detroit        7   9  0\n" +
+                "Minnesota      5  10  1");
+    }
+}
