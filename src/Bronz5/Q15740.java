974b173a3d8ae31df4c521545dff6733d036a388
heebum99 <heebum9955@naver.com>
2022-11-14 13:41:16 +0900

    15740번 - A+B

---
+++ src/Bronz5/Q15740.java
@@ -0,0 +1,13 @@
+package Bronz5;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Q15740 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigInteger num1 = scanner.nextBigInteger();
+        BigInteger num2 = scanner.nextBigInteger();
+        System.out.println(num1.add(num2));
+    }
+}
