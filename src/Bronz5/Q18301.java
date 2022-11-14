3d0afbf78326f9974fb8a07e51479b1442221d4b
heebum99 <heebum9955@naver.com>
2022-11-14 17:14:49 +0900

    18301번 - Rats

---
+++ src/Bronz5/Q18301.java
@@ -0,0 +1,15 @@
+package Bronz5;
+
+import java.util.Scanner;
+
+public class Q18301 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N1 = scanner.nextInt();
+        int N2 = scanner.nextInt();
+        int N3 = scanner.nextInt();
+        int N = ((N1 + 1) * (N2 + 1) / (N3 + 1)) - 1;
+
+        System.out.println(N);
+    }
+}
