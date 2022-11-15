ae9e7c8762dbfb02e50f7c58fc20c4898997a43f
heebum99 <heebum9955@naver.com>
2022-11-15 16:38:38 +0900

    24078번 - 余り (Remainder)

---
+++ src/Bronz5/Q24078.java
@@ -0,0 +1,12 @@
+package Bronz5;
+
+import java.util.Scanner;
+
+public class Q24078 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int X = scanner.nextInt();
+        System.out.println(X%21);
+    }
+}
