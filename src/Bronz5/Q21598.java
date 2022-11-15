f10a5b6e03fe9a23e6640a6e4dcd60d706ad3de9
heebum99 <heebum9955@naver.com>
2022-11-15 15:54:40 +0900

    21598번 - SciComLove

---
+++ src/Bronz5/Q21598.java
@@ -0,0 +1,14 @@
+package Bronz5;
+
+import java.util.Scanner;
+
+public class Q21598 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            System.out.println("SciComLove");
+        }
+    }
+}
