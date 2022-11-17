fdc32226c670cfd1c8071040f5d3d606940af962
heebum kim <108645814+heebum99@users.noreply.github.com>
2022-11-17 19:02:48 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 256 ms, Memory: 18700 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int hour = scanner.nextInt(); //현재 시간
+        int minute = scanner.nextInt(); //현재 분
+        int second = scanner.nextInt(); //현재 초
+        int lead_time = scanner.nextInt(); //필요 시간(초 단위로)
+        int total_second = hour * 3600 + minute * 60 + second + lead_time; //초 단위로 변경
+
+        hour = total_second / 3600 % 24;
+        minute = total_second %3600 /60 %60;
+        second = total_second % 60;
+
+        System.out.println(hour + " " + minute + " " + second);
+
+    }
+}
