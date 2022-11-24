4b496a0e2f4bd6a5b16bca81c5ef7de2d442c132
heebum99 <heebum9955@naver.com>
2022-11-24 23:50:41 +0900

    3765번 - Celebrity jeopardy

---
+++ src/Bronze4/Q3765.java
@@ -0,0 +1,14 @@
+package Bronze4;
+
+import java.util.Scanner;
+
+public class Q3765 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNextLine()){
+            String s = scanner.nextLine();
+            System.out.println(s);
+        }
+    }
+}
