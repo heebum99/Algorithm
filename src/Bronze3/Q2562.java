270bbdc20e2c9f1a2337cf0412462ad7316b5137
heebum99 <heebum9955@naver.com>
2022-11-29 17:37:45 +0900

    2562번 - 최댓값

---
+++ src/Bronze3/Q2562.java
@@ -0,0 +1,26 @@
+package Bronze3;
+
+import java.io.*;
+
+public class Q2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] numbers = new int[9];
+        int max = 0;
+        int index = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
+            if (max < numbers[i]) {
+                max = numbers[i];
+                index = i + 1;
+            }
+        }
+        br.close();
+        bw.write(Integer.toString(max));
+        bw.newLine();
+        bw.write(Integer.toString(index));
+        bw.flush();
+        bw.close();
+    }
+}
