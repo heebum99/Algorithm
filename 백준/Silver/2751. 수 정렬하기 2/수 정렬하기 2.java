c734139ebe6fc4afd0900fa57e6637a3f8110cb6
heebum kim <108645814+heebum99@users.noreply.github.com>
2023-03-02 13:00:12 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1584 ms, Memory: 121080 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.Arrays;
+
+//첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다.
+//이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine()); //주어진 N개의 수
+        int ary[] = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            ary[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(ary); //오름차순 정렬
+
+        for (int i = 0; i < N; i++) {
+            if (i >= 1 && ary[i] == ary[i - 1]) {
+                continue;
+            }
+            bw.write(Integer.toString(ary[i]));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
