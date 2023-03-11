8e44bbaecaac6fe63a2b3cfd5012ed0524bece2e
heebum kim <108645814+heebum99@users.noreply.github.com>
2023-03-11 11:40:21 +0900

    [Bronze V] Title: 오늘 날짜, Time: 196 ms, Memory: 17256 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(LocalDate.now());
+    }
+}
