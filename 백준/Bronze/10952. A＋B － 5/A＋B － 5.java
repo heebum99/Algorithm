e619cc7e6cf0534a5e2ce54d10756b2e4d49e67d
heebum kim <108645814+heebum99@users.noreply.github.com>
2023-03-11 11:43:17 +0900

    [Bronze V] Title: A+B - 5, Time: 232 ms, Memory: 18012 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A, B;
+        do {
+            A = scanner.nextInt();
+            B = scanner.nextInt();
+
+            if (A == 0 && B == 0) {
+                System.exit(0);
+            }
+
+            System.out.println(A + B);
+        } while (A != 0 && B != 0);
+    }
+}
