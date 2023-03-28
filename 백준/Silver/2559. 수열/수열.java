2622c10b7500241ff00d28a2f672f6fa7079e743
heebum kim <108645814+heebum99@users.noreply.github.com>
2023-03-28 17:49:01 +0900

    [Silver III] Title: 수열, Time: 260 ms, Memory: 23136 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -11,26 +11,33 @@ public class Main {
         int N = Integer.parseInt(st.nextToken()); //전체 날짜 수
         int K = Integer.parseInt(st.nextToken()); //연속된 날짜 수
 
-        long arr[] = new long[N];
+        long arr[] = new long[N +1];
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        long max = (-100) * K;
+        long max = (-100) * K; //최소 값으로 지정
         int start_idx = 0;
         int end_idx = start_idx + K;
-        while (end_idx <= N) {
-            long sum = 0;
-            for (int i = start_idx; i < end_idx; i++) {
-                sum += arr[i];
-            }
+        long sum = 0;
+        long count = 1;
 
+        for (int i = start_idx; i < end_idx; i++) {
+            sum += arr[i];
+        }
+
+        while (true) {
             if (max < sum) {
                 max = sum;
             }
-            start_idx++;
-            end_idx++;
+            if (count < N - K + 1) {
+                sum -= arr[start_idx++];
+                sum += arr[end_idx++];
+            } else{
+                break;
+            }
+            count++;
         }
         System.out.println(max);
     }
