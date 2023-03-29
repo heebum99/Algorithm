62f64dd7de94118814f529a13a72a507fe9ed666
heebum kim <108645814+heebum99@users.noreply.github.com>
2023-03-30 01:30:42 +0900

    [Bronze II] Title: 수 정렬하기, Time: 132 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,20 +6,61 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int[] ary = new int[N];
+
+
+        //버블 정렬을 2중 for문이 아닌 1중 for문으로 구현해 시간 효율 극대화
+        boolean[] check = new boolean[2001]; //-1000 ~ 1000
 
         for (int i = 0; i < N; i++) {
-            ary[i] = Integer.parseInt(br.readLine());
+            check[Integer.parseInt(br.readLine()) + 1000] = true;
         }
 
-        Arrays.sort(ary);
-
-        for (int i = 0; i < N; i++) {
-            bw.write(Integer.toString(ary[i])+"\n");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < check.length; i++) {
+            if (check[i]) {
+//                bw.write(Integer.toString(i - 1000) + "\n");
+                sb.append(i - 1000).append("\n");
+            }
         }
+        System.out.println(sb);
 
-        bw.flush();
-        bw.close();
-        br.close();
+        //버블 정렬 직접 구현
+//        int[] arr = new int[N];
+//
+//        for (int i = 0; i < N; i++) {
+//            arr[i] = Integer.parseInt(br.readLine());
+//        }
+//        int tmp;
+//        for (int i = 0; i < N - 1; i++) {
+//            for (int j = 0; j < N - 1 - i; j++) {
+//                if (arr[j] > arr[j + 1]) {
+//                    tmp = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = tmp;
+//                }
+//            }
+//        }
+//
+//        for (int i = 0; i < N; i++) {
+//            bw.write(Integer.toString(arr[i])+"\n");
+//        }
+
+
+        //Arrays.sort()이용
+//        int N = Integer.parseInt(br.readLine());
+//        int[] ary = new int[N];
+//
+//        for (int i = 0; i < N; i++) {
+//            ary[i] = Integer.parseInt(br.readLine());
+//        }
+//
+//        Arrays.sort(ary);
+//
+//        for (int i = 0; i < N; i++) {
+//            bw.write(Integer.toString(ary[i])+"\n");
+//        }
+//        bw.flush();
+//        bw.close();
+//        br.close();
     }
 }
