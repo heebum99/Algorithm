d35c2ec335218acb552cd78c5b55f5c3db2cc806
heebum kim <108645814+heebum99@users.noreply.github.com>
2023-04-28 21:51:05 +0900

    [Silver III] Title: 소수 구하기, Time: 276 ms, Memory: 24092 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,5 +1,6 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 //M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
@@ -7,7 +8,41 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        //인덱스 값을 이용한 에라토스테네스의 체
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] prime = new int[N + 1];
+
+        for (int i = 2; i <= N; i++) { //배열의 인덱스 값으로 초기화
+            prime[i] = i;
+        }
+
+        //소수가 아닌 경우 인덱스 값에서 0으로 바꿔준다.
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (prime[i] == 0) { //소수인 경우
+                continue;
+            }
+
+            for (int j = i * i; j <= N; j += i) { //배수를 모두 0으로 만들기
+                prime[j] = 0;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = M; i <= N; i++) {
+            if (prime[i] != 0) {
+                sb.append(i).append("\n");
+            }
+        }
+
+        System.out.print(sb);
+        br.close();
+
+        //boolean을 이용한 에라토스테네스의 체
+        /*BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int M = Integer.parseInt(st.nextToken());
@@ -33,6 +68,6 @@ public class Main {
         }
         bw.flush();
         bw.close();
-        br.close();
+        br.close();*/
     }
 }
