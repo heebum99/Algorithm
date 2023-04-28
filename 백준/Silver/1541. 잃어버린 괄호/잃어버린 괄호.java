ad49615b4da307b75acc53697ccc97e730154432
heebum kim <108645814+heebum99@users.noreply.github.com>
2023-04-28 21:36:22 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,13 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
+    static int ans = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int sum = Integer.MAX_VALUE; //첫 번째 수인지 여부를 확인하기 위해서 설정
+        //split 사용
+        String s = br.readLine();
+
+        //-를 기준으로 나누기
+        String[] sub = s.split("-");
+
+        for (int i = 0; i < sub.length; i++) {
+            int tmp = mySum(sub[i]);
+
+            if (i == 0) { //첫번째 수만 더해준다.
+                ans += tmp;
+            } else { //나머지 수는 빼준다.
+                ans -= tmp;
+            }
+        }
+
+        System.out.println(ans);
+        br.close();
+
+        //StringTokenizer 사용
+        /*int sum = Integer.MAX_VALUE; //첫 번째 수인지 여부를 확인하기 위해서 설정
 
         //결과가 최소로 나오려면 덧셈 먼저 수행 후 뺄셈 수행
         StringTokenizer sub = new StringTokenizer(br.readLine(), "-");
@@ -26,6 +47,15 @@ public class Main {
             }
         }
         System.out.println(sum);
-        br.close();
+        br.close();*/
+    }
+
+    private static int mySum(String s) {
+        int sum = 0;
+        String[] tmp = s.split("[+]"); //+의 경우에는 허상연산자로 +만 써주면 안되고 //+ 나 [+]로 표현해줘야함
+        for (int i = 0; i < tmp.length; i++) {
+            sum += Integer.parseInt(tmp[i]);
+        }
+        return sum;
     }
 }
