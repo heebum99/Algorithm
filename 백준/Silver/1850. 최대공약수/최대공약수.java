c389fde876f5efda804179244594065fc99159cd
heebum kim <108645814+heebum99@users.noreply.github.com>
2023-06-27 13:35:53 +0900

    [Silver I] Title: 최대공약수, Time: 312 ms, Memory: 38900 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long result = gcd(A, B);
+
+        for (long i = 0; i < result; i++) {
+            sb.append("1");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
