43f03c5f4f6a0dea27a2fa8ce0cb909ba7568950
heebum kim <108645814+heebum99@users.noreply.github.com>
2023-07-10 17:03:42 +0900

    [Silver III] Title: 피보나치 함수, Time: 124 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -3,24 +3,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    static int zero, one, sum;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        int zero[] = new int[41];
-        int one[] = new int[41];
-        zero[0] = 1;
-        one[0] = 0;
-        zero[1] = 0;
-        one[1] = 1;
-        for (int i = 2; i < 41; i++) {
-            zero[i] = zero[i - 1] + zero[i - 2];
-            one[i] = one[i - 1] + one[i - 2];
-        }
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < T; i++) {
+//        int[] zero = new int[41];
+//        int[] one = new int[41];
+//        zero[0] = 1;
+//        one[0] = 0;
+//        zero[1] = 0;
+//        one[1] = 1;
+//        for (int i = 2; i < 41; i++) {
+//            zero[i] = zero[i - 1] + zero[i - 2];
+//            one[i] = one[i - 1] + one[i - 2];
+//        }
+//        for (int i = 0; i < T; i++) {
+//            int N = Integer.parseInt(br.readLine());
+//            sb.append(zero[N]).append(" ").append(one[N]).append("\n");
+//        }
+//        System.out.print(sb);
+        while (T-- > 0) {
             int N = Integer.parseInt(br.readLine());
-            sb.append(zero[N]).append(" ").append(one[N]).append("\n");
+            fibonacci(N);
+            sb.append(zero).append(" ").append(one).append("\n");
         }
         System.out.print(sb);
+    }
+
+    private static void fibonacci(int n) {
+        //피보나치 규칙성
+        // => N에 대한 0 호출 횟수 == N-1에 대한 1의 호출 횟수
+        // => N에 대한 1 호출 횟수 == N-1에 대한 0의 호출 횟수 + N-1에 대한 1의 호출 횟수
+        zero = 1;
+        one = 0;
+        sum = 1;
+
+        for (int i = 1; i <= n; i++) {
+            zero = one;
+            one = sum;
+            sum = zero + one;
+        }
     }
 }
