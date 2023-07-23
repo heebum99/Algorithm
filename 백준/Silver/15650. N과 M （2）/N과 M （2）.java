25539f6fa861901c81a6cfdc15d285e1d27414c7
heebum kim <108645814+heebum99@users.noreply.github.com>
2023-07-23 18:39:02 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+
+        //백트래킹으로 구현
+        DFS(1, 0);
+        System.out.print(sb);
+    }
+
+    private static void DFS(int at, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = at; i <= N; i++) {
+            arr[depth] = i;
+            DFS(i + 1, depth + 1); //자식 노드 호출
+        }
+    }
+}
