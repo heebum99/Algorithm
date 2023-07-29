b603ab1ce73a87b9c80e4389c60eeb033be27e57
heebum kim <108645814+heebum99@users.noreply.github.com>
2023-07-29 14:08:47 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,36 +1,32 @@
-//자연수 N과 정수 K가 주어졌을때 이항 계수를 구해라.
-//이항계수 공식 => n!/k!(n-k)!
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[][] dp;
+    static int N, K;
+
     public static void main(String[] args) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        String s = br.readLine();
-        int result = 1; //이항계수 계산 결과값
-
-        StringTokenizer st = new StringTokenizer(s, " ");
-        int N = Integer.parseInt(st.nextToken()); //자연수
-        int K = Integer.parseInt(st.nextToken()); //정수
-
-        //이항계수 공식 => n!/k!(n-k)!
-        result = (factorial(N) / (factorial(N - K) * factorial(K)));
-        bw.write(Integer.toString(result));
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    public static int factorial(int num) { //팩토리얼 재귀 함수
-        int result = 1;
-        while (num > 1) {
-            result *= num;
-            num--;
+        dp = new int[N + 1][N + 1]; //배열 크기가 [N+1][K+1]이 아닌 [N+1][N+1]로, 헷갈리지 말자
+        for (int i = 0; i <= N; i++) { //조합 배열 초기화
+            dp[i][0] = 1;
+            dp[i][1] = i;
+            dp[i][i] = 1;
         }
-        return result;
+
+        //조합 점화식 dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
+        for (int i = 2; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                //j==i인 경우는 위에서 초기화를 했기에 따로 진행x
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+            }
+        }
+        System.out.println(dp[N][K]);
     }
 }
